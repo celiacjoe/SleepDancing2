f5bca744a173df2e22daa19ef3218da656754949
Louis Cortes <cortes.lou@gmail.com>
2022-11-24 13:58:19 +0100

    volumedanseur

--- SD2_UnityProject/Assets/Shader/NebulaRender.cs
+++ SD2_UnityProject/Assets/Shader/NebulaRender.cs
@@ -12,7 +12,10 @@ public class NebulaRender : MonoBehaviour
     int handle_main2;
     [Space(20)]
     [Header("PositionSetting")]
+    [Range(0, 1)]
     public float transition1;
+    [Range(0, 1)]
+    public float transition2;
     public Vector3 Pos;
     public Vector3 PosL;
     public float Focal;
@@ -74,7 +77,15 @@ public class NebulaRender : MonoBehaviour
         compute_shader.SetFloat("_resy", _resy);
         compute_shader.SetFloat("_resx2", _resx2);
         compute_shader.SetFloat("_resy2", _resy2);
-      //  compute_shader.SetTexture(handle_main, "reader3", C);
+        compute_shader.SetFloat("_reg1", reg1);
+        compute_shader.SetFloat("_reg2", reg2);
+        compute_shader.SetFloat("_reg3", reg3);
+        compute_shader.SetFloat("_reg4", reg4);
+        compute_shader.SetFloat("_co1", Co1);
+        compute_shader.SetFloat("_co2", Co2);
+        compute_shader.SetFloat("_co3", Co3);
+        compute_shader.SetVector("_VolumeColor", VolumeColor);
+        //  compute_shader.SetTexture(handle_main, "reader3", C);
     }
 
     void Update()
@@ -84,23 +95,18 @@ public class NebulaRender : MonoBehaviour
         compute_shader.SetTexture(handle_main, "reader", noise);
         compute_shader.SetTexture(handle_main2, "reader", noise);
         compute_shader.SetTexture(handle_main2, "reader2", A);
-        compute_shader.SetFloat("_reg1", reg1);
-        compute_shader.SetFloat("_reg2", reg2);
-        compute_shader.SetFloat("_reg3", reg3);
-        compute_shader.SetFloat("_co1", Co1);
-        compute_shader.SetFloat("_co2", Co2);
-        compute_shader.SetFloat("_co3", Co3);
         compute_shader.SetFloat("_t1", t1);
         compute_shader.SetFloat("_t2", t2);
         compute_shader.SetFloat("_t3", t3);
         compute_shader.SetFloat("_t4", t4);
         compute_shader.SetFloat("_transition1", transition1);
+        compute_shader.SetFloat("_transition2", transition2);
         compute_shader.SetVector("_position", Pos);
         compute_shader.SetVector("_positionL", PosL);
         compute_shader.SetFloat("_focal", Focal);
         compute_shader.SetFloat("_chro1", Chro1);
-        compute_shader.SetFloat("_reg4", reg4);
         compute_shader.SetFloat("_blur", Blur);
+
         compute_shader.SetFloat("_Low", Low);
         compute_shader.SetFloat("_SLow", SLow);
         compute_shader.SetFloat("_TLow", TLow);
@@ -110,8 +116,8 @@ public class NebulaRender : MonoBehaviour
         compute_shader.SetFloat("_High", High);
         compute_shader.SetFloat("_SHigh", SHigh);
         compute_shader.SetFloat("_THigh", THigh);
+
         compute_shader.SetFloat("_time", Time.time);
-        compute_shader.SetVector("_VolumeColor", VolumeColor);
         compute_shader.SetFloat("_mousex", Input.mousePosition.x / Display.main.systemWidth);
         compute_shader.SetFloat("_mousey", Input.mousePosition.y / Display.main.systemHeight);
         compute_shader.SetTexture(handle_main, "writer", A);
---
+++ SD2_UnityProject/Assets/Shader/VolumeDanceur.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class VolumeDanceur : MonoBehaviour
+{
+    public ComputeShader compute_shader;
+    public Material material;
+    public Texture D;
+    RenderTexture A;
+    RenderTexture B;
+    
+    public WebCamTexture C;
+
+
+    int handle_main;
+
+    [Range(0, 1)]
+    public float Taille;
+    [Range(0, 1)]
+    public float Forme;
+    [Range(0, 1)]
+    public float Disparition;
+
+    public int _resx;
+    public RenderTextureFormat rtFormat = RenderTextureFormat.ARGBHalf;
+    public int _resy;
+
+   // public render script;
+
+
+    void Start()
+    {
+        A = new RenderTexture(_resx, _resy, 0,rtFormat);
+        A.enableRandomWrite = true;
+        A.Create();
+        B = new RenderTexture(_resx, _resy, 0, rtFormat);
+        B.enableRandomWrite = true;
+        B.Create();
+
+        handle_main =  compute_shader.FindKernel("CSMain");
+
+        C = new WebCamTexture();
+        C.Play();
+        compute_shader.SetTexture(handle_main, "reader3", D);
+    }
+
+    void Update()
+    {
+       
+        compute_shader.SetTexture(handle_main, "reader", A);
+        compute_shader.SetTexture(handle_main, "reader2", C);
+
+        compute_shader.SetFloat("_time", Time.time);
+        compute_shader.SetFloat("_taille", Taille);
+        compute_shader.SetFloat("_forme", Forme);
+        compute_shader.SetFloat("_disparition", Disparition);
+        compute_shader.SetFloat("_resx", _resx);
+        compute_shader.SetFloat("_resy", _resy);
+        compute_shader.SetTexture(handle_main, "writer", B);
+        compute_shader.Dispatch(handle_main, B.width / 8, B.height / 8, 1);
+        compute_shader.SetTexture(handle_main, "reader", B);
+        compute_shader.SetTexture(handle_main, "writer", A);
+        compute_shader.Dispatch(handle_main, B.width / 8, B.height / 8, 1);
+        material.SetTexture("_Name", B);
+        //script.C = B;
+
+
+    }
+}
