a14e639a7af4c04611d2dba3f5d4b1ac24bf0ce3
Louis Cortes <cortes.lou@gmail.com>
2022-12-01 13:27:48 +0100

    standar video deform

--- SD2_UnityProject/Assets/Scripts/OSC_Video.cs
+++ SD2_UnityProject/Assets/Scripts/OSC_Video.cs
@@ -23,8 +23,6 @@ namespace OscSimpl.Examples
         public float THigh;
         public float SHigh;
         public float facMerge;
-        //public Vector3 fac;
-        //public Material mat;
         public string Name5;
         public string Name6;
         public string Name7;
@@ -41,17 +39,20 @@ namespace OscSimpl.Examples
         public Master_Control script2 ;
         public Material final;
         public Material final2;
+        public bool StandardActivated;
         float v1;
-        float map(float Val, float minInit, float MaxInit, float MinFinal, float MaxFinal)
-        {
-            return MinFinal + (Val - minInit) * (MaxFinal - MinFinal) / (MaxInit - minInit);
-        }
-
+        float v1o;
+        float Tailleo;
+        float bvid;
+        float Power;
+        float diro;
+        float Saturation;
         float _incomingFloat;
         private int Nbr_portIn;
 
         void Start()
         {
+            StandardActivated = false;
             Nbr_portIn = _oscIn.port;
             if (!_oscIn) _oscIn = gameObject.AddComponent<OscIn>();
             _oscIn.Open(Nbr_portIn);
@@ -65,16 +66,43 @@ namespace OscSimpl.Examples
             SLow = Mathf.Lerp(SLow, Low, facMerge);
             SMid = Mathf.Lerp(SMid, Mid, facMerge);
             SHigh = Mathf.Lerp(SHigh, High, facMerge);
-           /*
-            scrpit1.Low = Low;
-            scrpit1.TLow = TLow;
-            scrpit1.SLow = SLow;
-            scrpit1.Mid = Mid;
-            scrpit1.TMid = TMid;
-            scrpit1.SMid = SMid;
-            scrpit1.High = High;
-            scrpit1.THigh = THigh;
-            scrpit1.SHigh = SHigh;     */
+            if (StandardActivated == false)
+            {
+
+                v1 = v1o;
+                final.SetFloat("Power", Power);
+                scrpit1.Taille = Tailleo;
+                final2.SetFloat("bvid", bvid);
+                scrpit1.dir = diro;
+                final.SetFloat("Saturation", Saturation);
+            }
+            if (StandardActivated == true)
+            {
+                v1 = 1;
+                final.SetFloat("Power", 0.5f);
+                scrpit1.Taille = 0.2f;
+                final2.SetFloat("bvid", 0.1f);
+                scrpit1.dir = -0.1f;
+                final.SetFloat("Saturation", 0.5f);
+
+            }
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                // Debug.Log("te");
+                if (StandardActivated == false)
+                {
+
+
+                    StandardActivated = true;
+                }
+
+                else
+                {
+
+                    StandardActivated = false;
+                }
+
+            }
 
         }
 
@@ -84,7 +112,6 @@ namespace OscSimpl.Examples
             _oscIn.MapFloat(Name2, Event2);
             _oscIn.MapFloat(Name3, Event3);
             _oscIn.MapFloat(Name4, Event4);
-            //_oscIn.Map(Name5, Event5);
             _oscIn.MapFloat(Name6, Event6);
             _oscIn.MapFloat(Name7, Event7);
             _oscIn.MapFloat(Name8, Event8);
@@ -113,28 +140,18 @@ namespace OscSimpl.Examples
         }
         public void Event4(float value)
         {
-            v1 = Mathf.Pow(value, 5) * 20+0.001f;
+            v1o = Mathf.Pow(value, 5) * 20+0.001f;
         }
-        /*void Event5(OscMessage message)
-        {
-            float fa;
-            float fb;
-            if (message.TryGet(0, out fa) && message.TryGet(1, out fb))
-            {
-                scrpit1.m1 = fa  ;
-                scrpit1.m2 = fb  ;
-            }
-            OscPool.Recycle(message);
-        }   */
+
         public void Event6(float value)
         {
-            final.SetFloat("Power", value);
-            scrpit1.Taille = value;
+            Power =value;
+            Tailleo = value;
      
         }
         public void Event7(float value)
         {
-            final2.SetFloat("bvid", value);
+            bvid = value;
             script2.SharedBlurValue = value;
         }
         public void Event8(float value)
@@ -144,8 +161,8 @@ namespace OscSimpl.Examples
         }
         public void Event9(float value)
         {
-            scrpit1.dir = (value-0.5f)*2;
-            final.SetFloat("Saturation", value);
+            diro = (value-0.5f)*2;
+            Saturation = value;
         }
         public void Event10(float value)
         {
